49efdc2958cf0f0ce1dcbaa8af10f855c262d23f
andressa <goesandressa35@gmail.com>
2024-09-17 21:29:53 -0300

    instalação django

--- projeto_faculdade/projeto_faculdade/settings.py
+++ projeto_faculdade/projeto_faculdade/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'projeto_faculdade.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': { 
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'cadastro', 
+        'USER':'root', 
+        'PASSWORD':'2005DREssa', 
+        'HOST': '127.0.0.1', 
+        'PORTA':'3306'
+   }
 }
 
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
